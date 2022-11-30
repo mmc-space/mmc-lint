4992b34295abf833dda7e7eef2f249f75f655100
daiqin <daiqin@laiye.com>
2022-11-30 16:42:52 +0800

    feat: 🐈 init

---
+++ packages/react/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: [
+    'plugin:react/recommended',
+    '@mmc-group/eslint-config-ts',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'jsx-quotes': [
+      'error',
+      'prefer-double',
+    ],
+    "react/jsx-indent":[2, 2],
+    "react/jsx-indent-props": [2, 2],
+    'react/react-in-jsx-scope': 'off',
+  },
+}
