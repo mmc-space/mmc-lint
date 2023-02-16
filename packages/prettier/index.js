47c266873deb28d108c40d4c496a8afad778075c
戴勤 <1046572917@qq.com>
2023-02-16 21:41:45 +0800

    feat: 🐱 prettier

---
+++ packages/prettier/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  printWidth: 80, //一行的字符数，如果超过会进行换行，默认为80
+  tabWidth: 2, // 一个 tab 代表几个空格数，默认为 2 个
+  singleQuote: true,
+  trailingComma: "all",
+  semi: false, // 行尾是否使用分号，默认为true
+  bracketSpacing: true, // 对象大括号直接是否有空格，默认为 true，效果：{ a: 1 }
+};
